91bf8fa2d531388e648af75141255c5d93547044
Su-u-un <tl071515@gmail.com>
2024-05-14 22:09:56 +0800

    1

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -17,7 +17,7 @@ module.exports = {
     module:{
         rules:[
             {
-                test: /.(ts?)|(tsx?)|(js?)$/,
+                test: /.(ts|tsx)$/,
                 exclude: /node_modules/,
                 use:{
                     loader: 'babel-loader',
